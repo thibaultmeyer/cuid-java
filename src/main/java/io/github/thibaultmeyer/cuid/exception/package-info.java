c55aa797b13348a01ad1a059fe88a5af1c613e8f
Thibault Meyer <meyer.thibault@gmail.com>
2023-03-19 11:42:56 +0100

    Javadoc Typo
    Signed-off-by: Thibault Meyer <meyer.thibault@gmail.com>

--- src/main/java/io/github/thibaultmeyer/cuid/exception/package-info.java
+++ src/main/java/io/github/thibaultmeyer/cuid/exception/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Contains the exceptions that can be thrown when generating or manipulating a CUID..
+ * Contains the exceptions that can be thrown when generating or manipulating a CUID.
  *
  * @since 2.0.0
  */
